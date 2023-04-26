2639717712a1d8477de33b72a44927a0a6251d85
Jakob Bossek <info@jakobbossek.de>
2023-04-26 12:32:57 +0200

    improve css and container layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,42 @@
 $(document).ready(function() {
+  // smooth on-hover tooltips
   $("a[title], abbr[title], img[title]").tooltip();
+
+  // search for FAQ stufff
+  $('.faq_question').click(function(e) {
+    $(this).next('.faq_answer').toggle();
+    $(this).toggleClass('faq_open');
+  });
+
+  // smooth scroll if scroll destinations are fixed DOM IDs
+  $("#menu a").click(function(e) {
+    if (this.hash !== "") {
+      // get the destination and prevent default reaction
+      var hash = this.hash;
+      e.preventDefault();
+
+      // now smoothly scroll
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 300, function() {
+        window.location.hash = hash;
+      });
+    }
+  });
+
+  /* Toggle mobile menu */
+  const toggle = document.querySelector(".toggle");
+  const menu = document.querySelector("#menu");
+
+  function toggleMenu() {
+    if (menu.classList.contains("active")) {
+      menu.classList.remove("active");
+      toggle.innerHTML = "<i class='fas fa-bars'></i>";
+    } else {
+      menu.classList.add("active");
+      toggle.innerHTML = "<i class='fas fa-times'></i>";
+    }
+  }
+  toggle.addEventListener("click", toggleMenu, false);
 });
 
-/* Toggle mobile menu */
-const toggle = document.querySelector(".toggle");
-const menu = document.querySelector("#menu");
-
-function toggleMenu() {
-  if (menu.classList.contains("active")) {
-    menu.classList.remove("active");
-    toggle.innerHTML = "<i class='fas fa-bars'></i>";
-  } else {
-    menu.classList.add("active");
-    toggle.innerHTML = "<i class='fas fa-times'></i>";
-  }
-}
-toggle.addEventListener("click", toggleMenu, false);
